9f1243f78d696bbd750a6cd83d1f867ed1d48c2a
DillTheBeast <dillonmaltese@gmail.com>
2023-09-20 13:41:44 -0400

    a

--- Shapes.java
+++ Shapes.java
@@ -2,15 +2,34 @@ import java.util.Scanner;
 
 public class Shapes {
     public static void main(String[] args) {
-        //Triangle
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-
+        //Triangle
+        /*
         for (int y = 0; y < n; y++) {
             for (int x = 0; x < y+1; x++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
+        */
+        //Pyramid
+        for (int y = 1; y <= n; y++) {
+            // Print spaces
+            for (int x = 1; x <= n - y; x++) {
+                System.out.print(" ");
+            }
+            
+            // Print stars
+            for (int x = 1; x <= y; x++) {
+                System.out.print("*");
+                if (x != y) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
     }
+
 }
