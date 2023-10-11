e47e67126c112e9410e135ed7fb57690a191b055
DillTheBeast <dillonmaltese@gmail.com>
2023-10-11 14:22:34 -0400

    Create Classes.java

---
+++ Classes.java
@@ -0,0 +1,14 @@
+public class Classes {
+    public static void main(String[] args) {
+        Classes red = new Classes();
+        red.among(5);
+    }
+
+    public void among(int a) {
+        if(a > 0) {
+            int i = 0;
+            System.out.println(i);
+        }
+        
+    }
+}
