d781b65a89f8ce2d1f4d0c6ce3531b4084a9868f
DillTheBeast <dillonmaltese@gmail.com>
2023-10-25 14:05:32 -0400

    Create Problems.java

---
+++ Problems.java
@@ -0,0 +1,7 @@
+public class Problems {
+    //Take in a string of 5 numbers
+    //Can be double or float
+    //String should look like "52834"
+    //Sort from least to greatest
+    //Goes to "23458"
+}
