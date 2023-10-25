b54b155763afd9fef56d7adaf8c8b4f56d1fcc47
DillTheBeast <dillonmaltese@gmail.com>
2023-10-25 10:02:02 -0400

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        String a = "Hi my name is Dillob";
+        for(int i = 0; i < a.length(); i++) {
+            if(a.charAt(i) == ' ') {
+                System.out.println("Test");
+            }
+    }
+}
