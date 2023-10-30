d2ab9ccfb367cc081eecd1ae15cfb3ba4614dc0a
DillTheBeast <dillonmaltese@gmail.com>
2023-10-30 09:31:58 -0400

    Update Cars.java

--- Cars.java
+++ Cars.java
@@ -3,42 +3,57 @@ public class Cars {
     //Non-Electric = time and gas
     //Hybrig = time gas and charge
     public static void main(String[] args) {
-        Electric car1 = new Electric(3, 2);
+        Electric car1 = new Electric(3, 2, "White");
         car1.info();
-        NonElectric car2 = new NonElectric(4, 5);
-        Hybrid car3 = new Hybrid(6, 7, 8);
+        NonElectric car2 = new NonElectric(4, 5, "Black");
+        car2.info();
+        Hybrid car3 = new Hybrid(6, 7, 8, "Blue");
+        car3.info();
     }
 }
 
 class Electric {
     int time;
     private int charge;
-    public Electric(int time, int charge) {
+    String color;
+    public Electric(int time, int charge, String color) {
         this.time = time;
         this.charge = charge;
+        this.color = color;
     }
     public void info() {
-        System.out.println("The charge is " + charge);
+        System.out.println("The charge is " + charge + " and the time is " + time + " and the color is " + color);
     }
 }
 
 class NonElectric {
     int time;
     int gas;
+    String color;
 
-    public NonElectric(int time, int gas) {
+    public NonElectric(int time, int gas, String color) {
         this.time = time;
         this.gas = gas;
+        this.color = color;
+    }
+
+    public void info() {
+        System.out.println("The gas is " + gas + " and the time is " + time + " and the color is " + color);
     }
 }
 class Hybrid {
     int time;
     int gas;
     int charge;
+    String color;
 
-    public Hybrid(int time, int gas, int charge) {
+    public Hybrid(int time, int gas, int charge, String color) {
         this.time = time;
         this.gas = gas;
         this.charge = charge;
+        this.color = color;
+    }
+    public void info() {
+        System.out.println("The gas is " + gas + " and the time is " + time + " and the charge is " + charge + " and the color is " + color);
     }
 }
