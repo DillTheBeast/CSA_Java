ffc521cbaf0033119f19e264344fc17e93602a46
DillTheBeast <dillonmaltese@gmail.com>
2023-11-06 09:31:47 -0500

    Create StringReplace.java

---
+++ StringReplace.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class StringReplace {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == ';') {
+
+            } else if(str.charAt(i).equals("@")) {
+
+            }
+        }
+        if(str.contains(";") || str.contains("@") || str.contains("!")) {
+
+        }
+    }
+}
