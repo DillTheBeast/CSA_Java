f7150e79de65fd83f1c1aa18d08ce85788006729
DillTheBeast <dillonmaltese@gmail.com>
2023-12-03 22:08:50 -0500

    Update AnagramArrayList.java

--- AnagramArrayList.java
+++ AnagramArrayList.java
@@ -71,7 +71,6 @@ public class AnagramArrayList {
         return areArraysEqual(charArray1, charArray2);
     }
     static char[] sortCharArray(char[] arr) {
-        // Simple sorting algorithm (e.g., bubble sort)
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
