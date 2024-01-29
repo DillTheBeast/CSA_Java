4585f546a9835e7b8f7dd0cd51642b1a04d51d63
DillTheBeast <dillonmaltese@gmail.com>
2024-01-29 13:00:00 -0500

    sdf

--- Test.java
+++ Test.java
@@ -1,10 +1,8 @@
 public class Test {
     public static void main(String[] args) {
-        String a = "Hi my name is Dillob";
-        for(int i = 0; i < a.length(); i++) {
-            if(a.charAt(i) == ' ') {
-                System.out.println("Test");
-            }
-        }
+        int a = 5;
+        int b = 8;
+        int c = 3;
+        System.out.println(b / c);
     }
 }
