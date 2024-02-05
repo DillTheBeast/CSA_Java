92778ff7b99e39c6b8fd8d5a7e745931ad47716e
DillTheBeast <dillonmaltese@gmail.com>
2024-02-04 19:28:28 -0500

    asd

--- Test.java
+++ Test.java
@@ -1,8 +1,11 @@
 public class Test {
     public static void main(String[] args) {
-        int one = 1;
-       int two = 2;
-       String zee = "Z";
-       System.out.println(one + two + zee);
+        int a = 1988;
+int b = 1990;
+       String claim = " that the world’s athletes " +
+                  "competed in Olympic Games in ";
+       String s = "It is " + true + claim + a +
+               " but " + false + claim + b + ".";
+       System.out.println(s);
     }
 }
