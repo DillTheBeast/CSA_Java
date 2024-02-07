3b9767bdfad35627b4b29c219cdae9db6395e6ad
DillTheBeast <dillonmaltese@gmail.com>
2024-02-07 10:09:01 -0500

    s

--- Quiz1Review.java
+++ Quiz1Review.java
@@ -2,34 +2,45 @@
 
 public class Quiz1Review {
     public static void main(String[] args) {
-        B b = new B(3);
-        b.s;
-        System.out.println(b.getInfo());
-        A a = new A(4);
+        B test = new B();
+        System.out.println(test.getInfo());
     }
 }
 
-class A {
-    private int a;
-    String s;
-    public A(int a1, String str) {
-        a = a1;
-        s = str;
-    }
+// class A {
+//     private int a;
+//     String s;
+//     public A(int a1, String str) {
+//         a = a1;
+//         s = str;
+//     }
 
-    public A(int a1) {
-        a = a1;
-    }
-    public int getInfo() {
-        return a;
+//     public A(int a1) {
+//         a = a1;
+//     }
+//     public int getInfo() {
+//         return a;
+//     }
+// }
+
+// class B extends A {
+//     public B(int b1) {
+//         super(b1);
+//     }
+//     public int getInfo() {
+//         return super.getInfo();
+//     }
+// }
+
+class A {
+    public String getInfo() {
+        return "ABC";
     }
 }
 
 class B extends A {
-    public B(int b1) {
-        super(b1);
-    }
-    public int getInfo() {
-        return super.getInfo();
+    public String B() {
+        super.getInfo();
+        return "XYZ";
     }
 }
