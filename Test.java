647b85c3b34fdaf437a49212e2f49a88e65b780f
DillTheBeast <dillonmaltese@gmail.com>
2024-02-16 17:26:05 -0500

    sa

--- Test.java
+++ Test.java
@@ -1,10 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        boolean b1 = true && (17 % 3 == 1);
-        boolean b2 = (true && false) || true;
-        if(b1 == b2) {
-            System.out.println("True");
-        }
+        boolean x = (5 < 8) == (5 == 8);
+        System.out.println(x);
 }
 
 }
