331297f06dced3c8234f634af7b8e93120630a47
DillTheBeast <dillonmaltese@gmail.com>
2024-02-15 21:05:02 -0500

    asd

--- Test.java
+++ Test.java
@@ -1,18 +1,10 @@
 public class Test {
     public static void main(String[] args) {
-        int[] s = {3, 4};
-        int b = 4;
-        test(s, b);
-        for(int i = 0; i < s.length; i++) {
-            System.out.println(s[i]);
+        boolean b1 = true && (17 % 3 == 1);
+        boolean b2 = (true && false) || true;
+        if(b1 == b2) {
+            System.out.println("True");
         }
-        System.out.println(b);
+}
 
-    }
-    public static void test(int[] a, int y) {
-        if (a.length > 1) {
-            a[1] = a[1] * 2;
-        }
-        y = y * 2;
-    }
 }
