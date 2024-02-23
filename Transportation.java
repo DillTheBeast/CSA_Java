1e82913cda6a556073d72d4cb38ffd6210c20c2a
DillTheBeast <dillonmaltese@gmail.com>
2024-02-23 13:56:06 -0500

    Update Transportation.java

--- Transportation.java
+++ Transportation.java
@@ -1,6 +1,15 @@
 public class Transportation {
     public static void main(String[] args) {
-        
+        //Car c1 = new Vehicle();
+        Car c2 = new Car();
+        Car c3 = new SportsCar();
+        Vehicle v1 = new Vehicle();
+        Vehicle v2 = new Car();
+        Vehicle v3 = new SportsCar();
+        //SportsCar s1 = new Vehicle();
+        //SportsCar s2 = new Car();
+        SportsCar s3 = new SportsCar();
+        c3.test(c3, s3);
     }
 }
 
