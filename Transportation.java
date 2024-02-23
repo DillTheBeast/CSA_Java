bc22a80c2c77e49db67a60fd8bf939bd3397e07f
DillTheBeast <dillonmaltese@gmail.com>
2024-02-23 13:02:27 -0500

    Create Transportation.java

---
+++ Transportation.java
@@ -0,0 +1,19 @@
+public class Transportation {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Vehicle {
+
+}
+
+class Car extends Vehicle {
+    public void test(Car c, SportsCar s) {
+
+    }
+}
+
+class SportsCar extends Car {
+
+}
