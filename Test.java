465326fdce1e5698b753a82a26a20a01bdbbda0b
DillTheBeast <dillonmaltese@gmail.com>
2024-03-07 21:03:39 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,10 +1,24 @@
 public class Test {
     public static void main(String[] args) {
-        int a = 5;
-int b = 8;
-int c = 3;
-System.out.println(a + b / c * 2);
-
-
+        System.out.println(reverseString("Test"));
     }
+    public static String reverseString(String str) {
+        String result = "";
+        for (int s = str.length() - 1; s >= 0; s--) {
+            result += str.charAt(s);
+        }
+        return result;
+    }
+
+    public static void palindromeChecker(String str) {
+        String newStr = str.toLowerCase();
+        newStr = removeSpaces(newStr);
+        String reversedStr = reverseString(newStr);
+        if (reversedStr.equals(newStr)) {
+            System.out.println(str + " is a palindrome");
+        } else {
+            System.out.println(str + " is not a palindrome");
+        }
+    }
+
 }
