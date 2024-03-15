bb158c31998876c58f25703323dda331ed75b9db
DillTheBeast <dillonmaltese@gmail.com>
2024-03-15 16:52:50 -0400

    asd

--- Test.java
+++ Test.java
@@ -1,7 +1,23 @@
 public class Test {
     public static void main(String[] args) {
-        boolean x = (5 < 8) == (5 == 8);
-        System.out.println(x);
+        A a = new B();
+    }
 }
 
+class A {
+    public void getInfo() {
+        System.out.println("A");
+    }
+}
+
+class B extends A {
+    public void getInfo() {
+        System.out.println("B");
+    }
+}
+
+class C extends B {
+    public void getInfo() {
+        System.out.println("C");
+    }
 }
