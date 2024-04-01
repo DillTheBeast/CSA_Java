6e7b3111c7b1ec857a70d2aba8ebf9bd8d799547
DillTheBeast <dillonmaltese@gmail.com>
2024-04-01 14:23:13 -0400

    asd

--- things.java
+++ things.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
+
 public class things {
     public static void main(String[] args) {
-        add(3, 2);
-    }
+        ArrayList<Double> conditionRating = new ArrayList<Double>();
+        conditionRating.add(9.84);
+        conditionRating.add(8.93);
+        conditionRating.add(7.65);
+        conditionRating.add(6.24);
+        conditionRating.remove(2);
+        conditionRating.set(2, 7.63);
+        System.out.println(conditionRating);
 
-    public static int add(int x, int y) {
-        return x + y;
     }
 }
